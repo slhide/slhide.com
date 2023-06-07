0909c638715573da2a67d6ed50c84ce46b1164da
Dinorigami <135609438+dinorigami-sleurpys@users.noreply.github.com>
2023-06-07 16:13:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function handleSubmit () {
     const email = document.getElementById('email').value;
     // to set into local storage
-    /* localStorage.setItem("NAME", email); */
+    localStorage.setItem("NAME", email);
     sessionStorage.setItem("NAME", email);
     return;
 }
