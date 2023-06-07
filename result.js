671e6c1a125546688dd756896084b0e829b96b53
Dinorigami <135609438+dinorigami-sleurpys@users.noreply.github.com>
2023-06-07 16:32:03 +0200

    Update result.js

--- result.js
+++ result.js
@@ -1,13 +1,7 @@
 window.addEventListener('load', () => {
 
-    // Via Query parameters - GET
     const params = (new URL(document.location)).searchParams;
     var email = params.get('email');
-    document.getElementById('name').value = email;
-    // Via local Storage
-    const email = localStorage.getItem('NAME'); 
-    
-    const email = sessionStorage.getItem('NAME');
     document.getElementById('name').value = email;
 
 })
