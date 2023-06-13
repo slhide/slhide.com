f060f3bebf8574092ab1da2bc1a12d7251dddfeb
Dinorigami <135609438+slhide@users.noreply.github.com>
2023-06-13 18:45:16 +0200

    Update result.js

--- result.js
+++ result.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
 
     const params = (new URL(document.location)).searchParams;
     var email = params.get('email');
-    if (email!="null"){
+    if (email!=null){
     document.getElementById('name').setAttribute("value",email);}
 
 })
